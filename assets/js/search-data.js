e0e9d1b28169d2a7917c95cb74390d0605dffd46
chenwenyan <1358384755@qq.com>
2025-03-28 12:59:28 +0000

    Deploying to gh-pages from @ chenwenyan/chenwenyan.github.io@829086741288daef5ff61cf1dc35d85c66d93035 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+              window.location.href = "/news/announcement_3/";
+            },},{id: "news-smiless-is-accepted-by-sc-2024-smile",
+          title: 'SMIless is accepted by SC 2024. :smile:',
+          description: "",
+          section: "News",},{id: "news-mudi-was-accepted-by-eurosys-2025",
+          title: 'Mudi was accepted by EuroSys 2025.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
